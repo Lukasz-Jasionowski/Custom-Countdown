34288462ecb8a61780d93e812f0d69301fd7094e
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-06-19 13:22:29 +0200

    Add ability to store/restore dates & correct typos

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -53,6 +54,11 @@ function updateCountdown(e) {
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
+    savedCountdown = {
+        title: countdownTitle,
+        date: countdownDate,
+    };
+    localStorage.setItem('countdown', JSON.stringify(savedCountdown));
     if (countdownDate === '') {
         alert('Please select a date for the countdown!')
     } else {
@@ -66,10 +72,24 @@ function reset() {
     completeEl.hidden = true;
     inputContainer.hidden = false;
     clearInterval(countdownActive);
-    countdownElTitle = '';
+    countdownTitle = '';
     countdownDate = '';
+    localStorage.removeItem('countdown');
+}
+
+function restorePreviousCountdown() {
+    if (localStorage.getItem('countdown')) {
+        inputContainer.hidden = true;
+        savedCountdown = JSON.parse(localStorage.getItem('countdown'));
+        countdownTitle = savedCountdown.title;
+        countdownDate = savedCountdown.date;
+        countdownValue = new Date(countdownDate).getTime();
+        updateDOM();
+    }
 }
 
 countdownForm.addEventListener('submit', updateCountdown);
 countdownBtn.addEventListener('click', reset);
 completeBtn.addEventListener('click', reset);
+
+restorePreviousCountdown();
